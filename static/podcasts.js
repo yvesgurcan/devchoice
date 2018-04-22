1e7ddd6654abfa17afb86e719a9b74d50bcf6a98
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-04-21 22:15:33 -0700

    build 1

---
+++ static/podcasts.js
@@ -0,0 +1,51 @@
+window.data = {
+  podcasts: [
+    {
+      episode: 'Ionic & Web Component.',
+      name: 'The Web Platform Podcast',
+      description: 'A good intro to the Ionic framework and what web components are.',
+      url: 'https://thewebplatformpodcast.com/133-ionic-web-components',
+      image: 'the_web_platform.png',
+      addedOn: '2018/05/22',
+    },
+    {
+      episode: 'Interviews with VPs and Hiring a Tester',
+      name: 'Soft Skills Engineering',
+      description: 'A quick approach on what to say or not to say during a job interview.',
+      url: 'https://softskills.audio/2018/04/06/episode-104-interviews-with-vps-and-hiring-a-tester/',
+      image: 'soft_skills_engineering.png',
+      addedOn: '2018/05/15',
+    },
+    {
+      episode: 'Imposter syndrome and the Dunning-Kruger effect',
+      name: 'PHP Round Table',
+      description: 'An interesting take on the imposter syndrome with a little bit of PHP flavor in it.',
+      url: 'https://www.phproundtable.com/episode/imposter-syndrome-and-the-dunning-kruger-effect',
+      addedOn: '2018/05/08',
+    },
+    {
+      episode: '23 Years of Ruby with Yukihiro Matsumoto',
+      name: 'The Changelog',
+      description: 'If you ever wondered how Ruby was invented, this show is for you! The creator of the famous OOP language himself talks about how everything started.',
+      url: 'https://changelog.com/podcast/202',
+      image: 'matz.jpeg',
+      addedOn: '2018/05/01',
+    },
+    {
+      episode: 'The state of CSS moving forward in 2018',
+      name: 'The Web Platform Podcast',
+      description: 'An insightful look at how CSS evolved, where it is at now, and the direction it is taking.',
+      url: 'https://thewebplatformpodcast.com/139-the-state-of-css-moving-forward-in-2018',
+      image: 'the_web_platform.png',
+      addedOn: '2018/04/24',
+    },
+    {
+      episode: 'React: The Big Picture',
+      name: 'JavaScript Jabber',
+      description: 'A great show to put React in perspective. The guests also compare the Facebook library with Vue and Angular.',
+      url: 'https://devchat.tv/js-jabber/jsj-304-react-the-big-picture',
+      image: 'javascript_jabber.jpg',
+      addedOn: '2018/04/17',
+    },
+  ],
+}
