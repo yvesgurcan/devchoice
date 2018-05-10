f58b87873c594c866a50c11354e44773b1d7d956
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-05-10 15:44:26 -0700

    Update podcasts.js

--- app/static/podcasts.js
+++ app/static/podcasts.js
@@ -22,7 +22,7 @@ window.data = {
       addedOn: '2018/06/26',
     },
     {
-      episode: 'Funding with Grants',
+      episode: 'Funding with grants',
       name: 'Pusuit Podcast',
       description: 'Listen to this short episode for tips on how to write grant applications and get funding for your tech projects.',
       url: 'http://pursuit.tech/2018/05/08/funding-with-grants-danielle-robinson/',
